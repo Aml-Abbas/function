822f7fed18b792bba317b910cae04a6cc70b6d75
Aml Abbas <aml.abbas@hotmail.com>
2018-03-05 22:50:26 +0100

    working

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 
-    var y;
-    var x= document.getElementById("a");
+var form=document.getElementById('form');
+form.addEventListener('submit',function(e){
+e.preventDefault();
 
-switch (x) {
-    case "Blue": 
-        y = "You are blue";
-    break;
-    case "Red":
-     y = "You are red";
-    break;
-    case  "Black":
-       y = "You are black";
-    break;
-    case "White":    
-      y = "You are white";
- break;
- default:
- y = "Plz, write a color";
-
-}
-function check (){
-    document.getElementById("demo") .innerHTML= y;
-}
+	if (document.getElementById('b').checked) {
+document.getElementById('demo').innerHTML="your color is blue";			
+		}
+		else if (document.getElementById('r').checked){
+			document.getElementById('demo').innerHTML="your color is red";
+		}
+		else if (document.getElementById('bl').checked){
+			document.getElementById('demo').innerHTML="your color is black";
+		}
+		else if (document.getElementById('w').checked){
+			document.getElementById('demo').innerHTML="your color is white";
+		}
+		else {
+			document.getElementById('demo').innerHTML="Plz, choose a color";
+		}
+	});
